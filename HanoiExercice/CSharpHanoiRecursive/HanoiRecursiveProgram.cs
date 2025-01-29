d5549b5720c465f4b2ba455812eb4648ec042eea
Citaeth <106487971+Citaeth@users.noreply.github.com>
2025-01-28 22:39:36 -0500

    Create HanoiRecursiveProgram.cs

---
+++ HanoiExercice/CSharpHanoiRecursive/HanoiRecursiveProgram.cs
@@ -0,0 +1,7 @@
+HanoiTowerRecursive.HanoiRecursiveClass hanoi = new();
+int DiskNumber=3;
+string Left="left";
+string Middle="middle";
+string Right="right";
+
+hanoi.Move(DiskNumber, Left, Middle, Right);
